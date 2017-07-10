047fb18dbdceaf9e46de6c5ebc7abdcd1ff2d34b
Martin DeMello <mdemello@google.com>
2017-07-10 00:43:41 -0700

    add wordvar and lettervar

---
+++ src/rust/src/lettervar.rs
@@ -0,0 +1,114 @@
+use constraint::OverlapConstraint;
+use std::mem;
+use std;
+use std::ffi::CString;
+
+extern crate glib_sys;
+
+// constraint between a letter square and one word in that position
+#[repr(C)]
+pub struct LetterVar {
+  letter_counts: [[i32; 256]; 2], // support for each letter from the across=0 or down=1 words
+  letters_allowed: [i32; 256], // letters that can appear in this word
+  num_letters_allowed: i32,
+  constraints: [*mut OverlapConstraint; 2],
+  stack: *mut glib_sys::GArray, // for backtracking
+  name: *mut glib_sys::GString,
+  pos: *mut u8
+}
+
+impl LetterVar {
+  unsafe fn new(chr: u8, p: *mut u8) -> LetterVar {
+    let f = if chr == 46 { 1 } else { 0 };
+    let mut out = LetterVar {
+        letter_counts: [[0; 256]; 2],
+        letters_allowed: [f; 256],
+        num_letters_allowed: 0,
+        constraints: [std::ptr::null_mut(); 2],
+        stack: glib_sys::g_array_new(
+            0, 0, mem::size_of::<LetterVar>() as u32),
+        name: std::ptr::null_mut(),
+        pos: p
+    };
+    out.letters_allowed[chr as usize] = 1;
+    out
+  }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn make_lettervar(chr: u8, p: *mut u8) -> *mut LetterVar {
+    Box::into_raw(Box::new(LetterVar::new(chr, p)))
+}
+    
+#[no_mangle]
+pub unsafe extern "C" fn lettervar_set_name(
+    lptr: *mut LetterVar,
+    aw: *mut glib_sys::GString, dw: *mut glib_sys::GString,
+    row: i32, col: i32) {
+    let mut l = Box::from_raw(lptr);
+    let mut cstr = CString::new("").unwrap();
+    let fmtstr =  CString::new("%s / %s (%d,%d)").unwrap();
+    l.name = glib_sys::g_string_new(cstr.as_ptr());
+    glib_sys::g_string_printf(l.name, fmtstr.as_ptr(), aw, dw, row, col);
+    Box::into_raw(l);
+}
+    
+#[no_mangle]
+pub unsafe extern "C" fn lettervar_set_constraints(
+    lptr: *mut LetterVar,
+    oca: *mut OverlapConstraint,
+    ocd: *mut OverlapConstraint
+    ) {
+    let mut l = Box::from_raw(lptr);
+    l.constraints[0] = oca;
+    l.constraints[1] = ocd;
+    Box::into_raw(l);
+}
+    
+#[no_mangle]
+pub unsafe extern "C" fn set_letter(lptr: *mut LetterVar) {
+    let mut l = Box::from_raw(lptr);
+    for i in 0..256 {
+        if l.letters_allowed[i] != 0 {
+            *(l.pos) = i as u8;
+            break
+        }
+    }
+    Box::into_raw(l);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn lettervar_letter_allowed(
+    lptr: *mut LetterVar,
+    i: u8) -> i32 {
+    let mut l = Box::from_raw(lptr);
+    let out = l.letters_allowed[i as usize];
+    Box::into_raw(l);
+    out
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn lettervar_set_letter_allowed(
+    lptr: *mut LetterVar,
+    i: u8,
+    t: i32) -> i32 {
+    let mut l = Box::from_raw(lptr);
+    l.letters_allowed[i as usize] = t;
+    if t == 0 {
+        l.num_letters_allowed -= 1;
+    } else {
+        l.num_letters_allowed += 1;
+    }
+    let out = l.num_letters_allowed;
+    Box::into_raw(l);
+    out
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn lettervar_num_letters_allowed(
+    lptr: *mut LetterVar) -> i32 {
+    let mut l = Box::from_raw(lptr);
+    let out = l.num_letters_allowed;
+    Box::into_raw(l);
+    out
+}
---
+++ src/rust/src/wordvar.rs
@@ -0,0 +1,73 @@
+use constraint::Constraint;
+use constraint::OverlapConstraint;
+use constraint::UniquenessConstraint;
+use lettervar::LetterVar;
+use lettervar;
+use std;
+
+#[repr(C)]
+pub struct wordlist;
+
+extern crate glib_sys;
+
+#[link(name = "arccc")]
+extern {
+    pub fn wordlist_swap_index_with_end(w: *mut glib_sys::GPtrArray, i: i32) -> *mut u8;
+
+    pub fn put_constraint_on_queue(c: *mut Constraint);
+}
+
+#[repr(C)]
+pub struct WordVar {
+    possible_values: *mut glib_sys::GPtrArray,
+    length: i32,
+    letters: *mut *mut LetterVar,
+    letter_counts: *mut *mut i32,
+    stack: *mut glib_sys::GPtrArray,
+    orthogonal_constraints: *mut *mut OverlapConstraint,
+    unique_constraint: *mut UniquenessConstraint,
+    name: *mut glib_sys::GString
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn wordlist_remove_index(wptr: *mut WordVar, index: i32) -> i32 {
+    let mut w = Box::from_raw(wptr);
+    let temp = wordlist_swap_index_with_end(w.possible_values, index);
+    // loop over characters, decrementing counts in corresponding lettervar
+    for i in 0 .. w.length {
+        let ix = temp.offset(i as isize);
+        let t = *(w.letter_counts.offset(i as isize));
+        let mut lc = t.offset(*ix as isize);
+        *lc = *lc - 1;
+        if *lc == 0  {
+            let oc = w.orthogonal_constraints.offset(i as isize);
+            let c = (**oc).constraint;
+            if c != std::ptr::null_mut() {
+                let l = (*c).l;
+                if lettervar::lettervar_letter_allowed(l, *ix) == 0 {
+                    continue;
+                }
+
+                let num = lettervar::lettervar_set_letter_allowed(l, *ix, 0);
+                if num == 0 {
+                    Box::into_raw(w);
+                    return 0
+                }
+
+                if num == 1 {
+                    lettervar::set_letter(l)
+                }
+
+                put_constraint_on_queue(*oc);
+            }
+        }
+    }
+
+    if (*w.possible_values).len == 1 {
+        put_constraint_on_queue(w.unique_constraint);
+    }
+
+    Box::into_raw(w);
+    return 1;
+}
+    
